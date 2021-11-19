d299bebc312eb3b91fd7303ace1c7dd8c17de1e7
Matt <m4tt1muspr1m3@gmail.com>
2021-11-18 19:12:17 -0600

    update to remove old artifacts and extend the sleep time to give time for dotnet to start up

--- InitScripts/startupScript.sh
+++ InitScripts/startupScript.sh
@@ -1,3 +1,5 @@
+rm ~/BlazorMirror/InitScripts/nohop.out &
+rm -r ~/BlazorMirror/InitScripts/nohop &
 nohup dotnet run --project ~/BlazorMirror/BlazorMirror.csproj &
-sleep 5
+sleep 15
 sensible-browser http://localhost:5000 --start-fullscreen
