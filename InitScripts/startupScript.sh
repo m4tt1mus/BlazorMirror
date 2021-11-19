d39461e36a506054d27c54bcde05af38a5a93154
Matt <m4tt1muspr1m3@gmail.com>
2021-11-19 00:06:26 -0600

    update startup script to put the nohop.out in an easily accessible location

--- InitScripts/startupScript.sh
+++ InitScripts/startupScript.sh
@@ -1,5 +1,4 @@
-rm /home/pi/BlazorMirror/InitScripts/nohop.out &
-rm -r /home/pi/BlazorMirror/InitScripts/nohop &
-nohup /home/pi/dotnet/dotnet run --project /home/pi/BlazorMirror/BlazorMirror.csproj &
+rm /home/pi/nohop.out &
+nohup /home/pi/dotnet/dotnet run --project /home/pi/BlazorMirror/BlazorMirror.csproj &> /home/pi/nohup.out &
 sleep 15
 sensible-browser http://localhost:5000 --start-fullscreen
